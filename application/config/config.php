4adc8da690d843848a63814d4304cb37ce692d87
Alexanderrikky <alexanderrikky10@gmail.com>
2024-06-19 10:42:08 +0700

    ubah config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/tukangin/';
+$config['base_url'] = 'http://localhost/tukangin.wp2/';
 
 /*
 |--------------------------------------------------------------------------
